b0cd16367fe280c16786d74aee939fd812e7cec3
Pranathi Peri <pranathi@graphite.dev>
2023-08-01 11:27:29 -0400

    reverting to 85e921d

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
+<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+=======
+import GameController from "./GameController";
+import "./styles.css";
+import React from "react";
+>>>>>>> d11832d ([GameController] Adding call to game controller in App.js)
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GameController />
     </div>
   );
 }
-
-export default App;
